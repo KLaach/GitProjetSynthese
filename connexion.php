58755c0d98ae39c26e41a027e40fabd4213172a5
KLaach <k.laachouri@gmail.com>
2017-11-02 13:33:03 +0100

    Premier commit : Page accueil

---
+++ connexion.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	 <title>Brain Fight - Connexion</title>
+       	 <meta charset="utf-8" />
+       	 <link rel="stylesheet" href="css/app.css" />
+    </head>
+    <body>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	 <title>Brain Fight</title>
+       	 <meta charset="utf-8" />
+       	 <link rel="stylesheet" href="css/app.css" />
+    </head>
+    <body>
+    	<header>
+    		<img src="image/titreApp.png" class="AppTitle">
+    	</header>
+    	<h1 class="Accueil"> <img src="image/logo.png" class="ImgAccueil"> </h1>
+	    <a class="TxtAccueil" href="connexion.php"> <img src="image/jouer.png" class="Jouer"> </a>
+    </body>
+</html>
